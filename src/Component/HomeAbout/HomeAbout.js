3a404a890f09334accdd2d780a76b3b98f35b510
Mayur <mdawange@techstitcher.com>
2022-12-01 19:42:02 +0530

    UseEffect Concept

--- src/Component/HomeAbout/HomeAbout.js
+++ src/Component/HomeAbout/HomeAbout.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./homeAbout.scss";
-import { Container } from "react-bootstrap";
+import { Alert, Button, Container } from "react-bootstrap";
 import aboutImage from "../../Assets/img/banner3.webp";
 
 const HomeAbout = () => {
+  const [state, setState] = useState(0);
+  const [Width, setWidth] = useState(window.screen.width);
+  const [show, setShow] = useState(null);
+
+  const actualWidth = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", actualWidth);
+
+    return () => {
+      window.removeEventListener("resize", actualWidth);
+    };
+  });
+
+  useEffect(() => {
+    document.title = `chats ${state} `;
+  });
+
   return (
     <>
       <section className="home_about">
@@ -24,12 +44,37 @@ const HomeAbout = () => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Recusandae iste dicta tenetur consectetur
                 </p>
-                <button>Apply Now</button>
+                <Button onClick={() => setState(state + 1)}>
+                  Counter Chat
+                </Button>
+                <span>{state}</span>
               </div>
             </div>
-            <div className="left_about_box col-lg-6">
-              <img src={aboutImage} alt="aboutImage" />
+            <div className="right_about_box col-lg-6">
+              <div className="inner_right">
+                <img src={aboutImage} alt="aboutImage" />
+                {/* <span className="screen_size">
+                  Your Screen Size <br /> {Width}px
+                </span> */}
+              </div>
             </div>
+          </div>
+          <Alert show={show} variant="success" className="my-5">
+            <Alert.Heading>Your Screen Size </Alert.Heading>
+            <p className="Width_screen">{Width}</p>
+
+            <div className="d-flex justify-content-end">
+              <Button onClick={() => setShow(false)} variant="outline-success">
+                Close
+              </Button>
+            </div>
+          </Alert>
+          <div className="screen_btn">
+            {!show && (
+              <Button onClick={() => setShow(true)} variant="success">
+                Check Your Screen Size
+              </Button>
+            )}
           </div>
         </Container>
       </section>
--- src/Component/UseEffect/UseEffect.js
+++ src/Component/UseEffect/UseEffect.js
@@ -3,18 +3,18 @@ import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 
 const UseEffect = () => {
-  const [count, setcount] = useState(0);
-  useEffect(() => {
-    // console.log("Hello UseEffect");
-    document.title = `Chats (${count})`;
-  });
+  const [Count, setCount] = useState(0);
+
+  useEffect = () => {
+    window.title = `chats (${Count})`;
+  };
   //   console.log("out side console");
   return (
     <div className="my-5">
       <Container className="col-lg-8 py-5 bg-info">
         Hello UseEffect Section
-        <h4>{count}</h4>
-        <Button onClick={() => setcount(count + 1)}>Counter</Button>
+        <h4>{Count}</h4>
+        <Button onClick={() => setCount(Count + 1)}>Counter</Button>
       </Container>
     </div>
   );
